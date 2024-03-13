2ba191b0db32754a132bb02e11565a390af4b171
Lee Cheneler <lacheneler@gmail.com>
2024-03-13 21:48:42 +0000

    initial commit

---
+++ src/instrumentation.node.ts
@@ -0,0 +1,15 @@
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "insightful",
+  }),
+  spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
+  serviceName: "insightful",
+});
+
+sdk.start();
